4ad7c0004c60e9d9070e47e3e2d5d67b3fac80ea
simonucl <simon011130@gmail.com>
2024-06-17 03:37:01 +0100

    update run

--- scripts/make_qa_data_from_retrieval_results.py
+++ scripts/make_qa_data_from_retrieval_results.py
@@ -25,62 +25,123 @@ from copy import deepcopy
 
 from tqdm import tqdm
 from xopen import xopen
+import os
 
 logger = logging.getLogger(__name__)
 
 
 def main(input_path, num_total_documents, gold_index, output_path):
 
-    if num_total_documents < 2:
-        raise ValueError(f"`num_total_documents` must be at least 2, got {num_total_documents}")
-    if gold_index < 0:
-        raise ValueError("`gold_index` must be at least 0")
-    if gold_index >= num_total_documents:
-        raise ValueError(f"`gold_index` must be less than `num_total_documents` ({num_total_documents})")
+    output_path = os.path.join(output_path, 'nq-open-{}-{}_total_documents_gold_at_{}.jsonl.gz')
+    # if num_total_documents < 2:
+    #     raise ValueError(f"`num_total_documents` must be at least 2, got {num_total_documents}")
+    # if gold_index < 0:
+    #     raise ValueError("`gold_index` must be at least 0")
+    # if gold_index >= num_total_documents:
+    #     raise ValueError(f"`gold_index` must be less than `num_total_documents` ({num_total_documents})")
 
     # Validate that we have at least num_total_documents for every example
-    with xopen(input_path) as fin:
-        for line in tqdm(fin):
-            qa_retrieval_result = json.loads(line)
-            example_num_documents = len([doc for doc in qa_retrieval_result["ctxs"] if doc["hasanswer"] is False])
-            if num_total_documents > example_num_documents:
-                raise ValueError(
-                    f"Requested `num_total_documents` {num_total_documents}, but found an input"
-                    f"example with only {example_num_documents} documents that don't contain the answer."
-                )
+    # with xopen(input_path) as fin:
+    max_length = input_path.split('/')[-1].split('_')[0]
+    segment_path = 'wiki_dump/wiki/segments/{}/{}/segments.json'
+    # with open(input_path) as fin:
+    #     for line in tqdm(fin):
+    #         qa_retrieval_result = json.loads(line)
+    #         example_documents = qa_retrieval_result['ctxs_id']
+    #         # example_num_documents = len([doc for doc in qa_retrieval_result["ctxs"] if doc["hasanswer"] is False])
+    #         if num_total_documents > len(example_documents):
+    #             raise ValueError(
+    #                 f"Requested `num_total_documents` {num_total_documents}, but found an input"
+    #                 f"example with only {len(example_documents)} documents that don't contain the answer."
+    #             )
 
-    num_output_examples = 0
-    with xopen(input_path) as fin, xopen(output_path, "w") as fout:
-        for line in tqdm(fin):
+    distractor_docs_and_gold = []
+    with open(input_path) as fin:
+        for ii, line in tqdm(enumerate(fin), total=2578):
             qa_retrieval_result = json.loads(line)
-            # Get documents that don't contain the answer
-            valid_distractors_with_retrieval_indices = [
-                (idx, doc) for idx, doc in enumerate(qa_retrieval_result["ctxs"]) if doc["hasanswer"] is False
-            ]
-            # Take the top `num_total_documents - 1` distractors
-            distractor_docs_with_retrieval_indices = deepcopy(
-                valid_distractors_with_retrieval_indices[: num_total_documents - 1]
-            )
-            for original_retrieval_index, distractor_doc in distractor_docs_with_retrieval_indices:
-                distractor_doc["original_retrieval_index"] = original_retrieval_index
-                distractor_doc["isgold"] = False
-            distractor_docs = [x[1] for x in distractor_docs_with_retrieval_indices]
-
-            content_selection_example = deepcopy(qa_retrieval_result)
+            distractor_docs_indices = qa_retrieval_result['ctxs_id'][: num_total_documents - 1]
+            distractor_docs = []
+            for distractor_id in distractor_docs_indices:
+                doc_id, line_id = distractor_id.split('/')[0], distractor_id.split('/')[1]
+                with open(segment_path.format(doc_id, max_length)) as f:
+                    # direct load line_id line
+                    for i, line in enumerate(f):
+                        if i == int(line_id):
+                            distractor_doc = json.loads(line)
+                            distractor_doc = {
+                                "title": distractor_doc["title"],
+                                "text": distractor_doc["texts"],
+                                "hasanswer": False,
+                                "isgold": False,
+                            }
+                            distractor_docs.append(distractor_doc)
+                            break
             gold_chunk = {
                 "title": qa_retrieval_result["nq_annotated_gold"]["title"],
                 "text": qa_retrieval_result["nq_annotated_gold"]["chunked_long_answer"],
                 "hasanswer": True,
                 "isgold": True,
             }
-            ctxs = distractor_docs
-            # Insert the gold chunk at thet specific index
-            ctxs.insert(gold_index, gold_chunk)
+            qa_retrieval_result.pop('ctxs_id')
+            distractor_docs_and_gold.append((qa_retrieval_result, distractor_docs, gold_chunk))
 
-            content_selection_example["ctxs"] = ctxs
-            fout.write(json.dumps(content_selection_example) + "\n")
-            num_output_examples += 1
-    logger.info(f"Wrote {num_output_examples} output examples")
+    for gold_i in gold_index:
+        with xopen(output_path.format(num_total_documents, max_length, gold_i), "w") as fout:
+            for qa_retrieval_result, distractor_docs, gold_chunk in distractor_docs_and_gold:
+                ctxs = distractor_docs
+                ctxs.insert(gold_i, gold_chunk)
+                qa_retrieval_result["ctxs"] = ctxs
+                fout.write(json.dumps(qa_retrieval_result) + "\n")
+            
+    # num_output_examples = 0
+    # with open(input_path) as fin, xopen(output_path, "w") as fout:
+    #     for line in tqdm(fin):
+    #         qa_retrieval_result = json.loads(line)
+    #         # Get documents that don't contain the answer
+    #         # valid_distractors_with_retrieval_indices = [
+    #         #     (idx, doc) for idx, doc in enumerate(qa_retrieval_result["ctxs"]) if doc["hasanswer"] is False
+    #         # ]
+    #         # # Take the top `num_total_documents - 1` distractors
+    #         # distractor_docs_with_retrieval_indices = deepcopy(
+    #         #     valid_distractors_with_retrieval_indices[: num_total_documents - 1]
+    #         # )
+    #         distractor_docs_indices = qa_retrieval_result['ctxs_id'][: num_total_documents - 1]
+    #         # for original_retrieval_index, distractor_doc in distractor_docs_indices:
+    #         #     distractor_doc["original_retrieval_index"] = original_retrieval_index
+    #         #     distractor_doc["isgold"] = False
+    #         # distractor_docs = [x[1] for x in distractor_docs_with_retrieval_indices]
+    #         distractor_docs = []
+    #         for distractor_id in tqdm(distractor_docs_indices, desc="Loading distractor documents"):
+    #             doc_id, line_id = distractor_id.split('/')[0], distractor_id.split('/')[1]
+    #             with open(segment_path.format(doc_id, max_length)) as f:
+    #                 # direct load line_id line
+    #                 for i, line in enumerate(f):
+    #                     if i == line_id:
+    #                         distractor_doc = json.loads(line)
+    #                         distractor_doc = {
+    #                             "title": distractor_doc["title"],
+    #                             "text": distractor_doc["text"],
+    #                             "hasanswer": False,
+    #                             "isgold": False,
+    #                         }
+    #                         distractor_docs.append(distractor_doc)
+    #                         break
+                
+    #         content_selection_example = deepcopy(qa_retrieval_result)
+    #         gold_chunk = {
+    #             "title": qa_retrieval_result["nq_annotated_gold"]["title"],
+    #             "text": qa_retrieval_result["nq_annotated_gold"]["chunked_long_answer"],
+    #             "hasanswer": True,
+    #             "isgold": True,
+    #         }
+    #         ctxs = distractor_docs
+    #         # Insert the gold chunk at thet specific index
+    #         ctxs.insert(gold_index, gold_chunk)
+
+    #         content_selection_example["ctxs"] = ctxs
+    #         fout.write(json.dumps(content_selection_example) + "\n")
+    #         num_output_examples += 1
+    # logger.info(f"Wrote {num_output_examples} output examples")
 
 
 if __name__ == "__main__":
@@ -104,6 +165,7 @@ if __name__ == "__main__":
         "--gold-index",
         help=("Index to place gold documents at. Must be 0 or greater and " "`num-total-documents - 1` or smaller"),
         type=int,
+        nargs="+",
         required=True,
     )
     parser.add_argument("--output-path", help="Path to write output data files", required=True)
@@ -112,3 +174,11 @@ if __name__ == "__main__":
     logger.info("running %s", " ".join(sys.argv))
     main(args.input_path, args.num_total_documents, args.gold_index, args.output_path)
     logger.info("finished running %s", sys.argv[0])
+
+"""
+python3 -u ./scripts/make_qa_data_from_retrieval_results.py \
+    --input-path wiki_dump/4096_new_ret_docs.jsonl \
+    --num-total-documents 20 \
+    --gold-index 0 4 9 14 19 \
+    --output-path qa_data/30_total_documents/
+"""
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,9 @@
 MODELS=(
-    /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-8B-Instruct
+    # /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3-8B-Instruct
     gradientai/Llama-3-8B-Instruct-262k
     mistralai/Mistral-7B-Instruct-v0.3
     microsoft/Phi-3-mini-128k-instruct
+    # microsoft/Phi-3-medium-128k-instruct
 )
 
 # Close book
@@ -61,17 +62,36 @@ for model in "${MODELS[@]}"; do
     #     --input-path qa_predictions/nq-open-oracle-${model}-oracle-doc-predictions.jsonl.gz \
     #     --output-path qa_predictions/nq-open-oracle-${model}-oracle-doc-predictions-scored.jsonl.gz
 
-    echo "Running model: $model in orcale doc setting"
-    python3 -u ./scripts/get_qa_responses.py \
-    --input-path qa_data/nq-open-all_oracle_docs.jsonl.gz \
-    --max-new-tokens 100 \
-    --num-gpus 2 \
-    --model $model \
-    --add_system_prompt \
-    --output-path qa_predictions/nq-open-oracle-${model}-all_oracle_docs-predictions.jsonl.gz
+    for gold_index in 0 4 9 14 19 ; do
+        echo "Running model: $model in open book setting with gold index: $gold_index"
+        python3 -u scripts/get_qa_responses.py \
+            --input-path qa_data/30_total_documents/nq-open-20-4096_total_documents_gold_at_${gold_index}.jsonl.gz \
+            --num-gpus 2 \
+            --max-new-tokens 100 \
+            --model $model \
+            --add_system_prompt \
+            --output-path qa_predictions/nq-open-20-4096-${model}-openbook-gold-index-${gold_index}-predictions.jsonl.gz
 
-    python3 -u scripts/evaluate_qa_responses.py \
-        --input-path qa_predictions/nq-open-oracle-${model}-all_oracle_docs-predictions.jsonl.gz \
-        --output-path qa_predictions/nq-open-oracle-${model}-all_oracle_docs-predictions-scored.jsonl.gz
+        python3 -u scripts/evaluate_qa_responses.py \
+            --input-path qa_predictions/nq-open-20-4096-${model}-openbook-gold-index-${gold_index}-predictions.jsonl.gz \
+            --output-path qa_predictions/nq-open-20-4096-${model}-openbook-gold-index-${gold_index}-predictions-scored.jsonl.gz
+    done
+done
 
+for model in "${MODELS[@]}"; do
+
+    for gold_index in 0 4 9 14 19 24 29; do
+        echo "Running model: $model in open book setting with gold index: $gold_index"
+        python3 -u scripts/get_qa_responses.py \
+            --input-path qa_data/30_total_documents/nq-open-30_2048_total_documents_gold_at_${gold_index}.jsonl.gz \
+            --num-gpus 2 \
+            --max-new-tokens 100 \
+            --model $model \
+            --add_system_prompt \
+            --output-path qa_predictions/nq-open-30_2048-${model}-openbook-gold-index-${gold_index}-predictions.jsonl.gz
+
+        python3 -u scripts/evaluate_qa_responses.py \
+            --input-path qa_predictions/nq-open-30_2048-${model}-openbook-gold-index-${gold_index}-predictions.jsonl.gz \
+            --output-path qa_predictions/nq-open-30_2048-${model}-openbook-gold-index-${gold_index}-predictions-scored.jsonl.gz
+    done
 done
